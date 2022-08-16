55f712f40e560355d0733e6baecf80b360424157
Steven Trujillo <potencyalmighty@outlook.com>
2022-08-16 17:14:07 -0600

    Was able to connect my input and likes to LocalStorage to maintain the state of both.

--- script.js
+++ script.js
@@ -6,63 +6,88 @@ const popup = document.getElementById("card-enlarge");
 const closePopupBtn = document.getElementById("close-popup");
 const hmm = document.getElementById("fuck");
 
-/*let storage = JSON.parse(localStorage.getItem('posts'));
+let card_id = 0;
 
-if(storage){
-    storage.forEach((store) => {
+let postStorage = JSON.parse(localStorage.getItem('posts'));
+let likeStorage = JSON.parse(localStorage.getItem('likes'))
+
+if(postStorage){
+    postStorage.forEach(store => {
         addNote(store)
+    })
+}
+
+/*if(likeStorage){
+    likeStorage.forEach(whoa => {
+        addNote(whoa)
     })
 }*/
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    addNote();
+})
+
+function addNote(store) {
+    if(likeStorage){
+        console.log(likeStorage.like)
+    }
     let el_id = new Date().getTime();
     const x = input.value;
+    let count = 0;
 
     const newCard = document.createElement("div");
-
+        newCard.classList.add("card")
         newCard.innerHTML = `
-        <div class="card">
-            <div id="content-${el_id}">
-            <div>
-                <img src="space.jpg" alt="space">
-            </div>
-            
-            <div class="card-header">
-                <h2>${x}</h2>
-            </div> 
-            </div>
-            <div class="btn-container" id="btn-container">
-                <div class="like-container">
-                    <button class="like-btn" id="like-${el_id}">Like</button>
-                    <span id="${el_id}"></span>
+            <div class="${store ? store.name : x}">
+                <div id="content-${card_id}">
+                <div>
+                    <img src="space.jpg" alt="space">
                 </div>
-                <button class="delete-btn" id="delete-${el_id}">Delete</button>
+            
+                <div class="card-header">
+                    <h2>${store ? store.name : x}</h2>
+                </div> 
+                </div>
+                <div class="btn-container" id="btn-container">
+                    <div class="like-container">
+                        <button class="like-btn" id="like-${card_id}">Like</button>
+                        <span class="${card_id}" id="${card_id}">${count}</span>
+                    </div>
+                    <button class="delete-btn" id="delete-${card_id}">Delete</button>
+                </div>
             </div>
-        </div>
-        `;
+            `;
 
         container.appendChild(newCard);
 
-        let content = document.getElementById(`content-${el_id}`);
+        let content = document.getElementById(`content-${card_id}`);
 
         content.addEventListener("click", () => {
             updatePopup(x)
         })
 
-        input.value = "";
+        let like = document.getElementById(`like-${card_id}`);
+        let num = document.getElementById(`${card_id}`);
 
-        let like = document.getElementById(`like-${el_id}`);
-        let num = document.getElementById(`${el_id}`);
-        let count = 0;
+        if(count === 0){
+            num.innerText = "";
+        }
+        if(likeStorage){
+            num.innerText = likeStorage[card_id].like
+        }
 
         like.addEventListener("click", () => {
             count++;
             num.innerText = count;
+            likeLs(count)
         })
 
-        let del = document.getElementById(`delete-${el_id}`);
+        let del = document.getElementById(`delete-${card_id}`);
 
         del.addEventListener("click", () => {
+            newCard.classList.remove("card")
             newCard.innerHTML = "";
         })
 
@@ -71,7 +96,13 @@ btn.addEventListener("click", () => {
         
         })
 
-})
+        card_id++
+        //updateLsId(card_id);
+        updateLs();
+        
+
+        input.value = "";
+}
 
 function updatePopup (x){
     hmm.innerHTML = "";
@@ -89,27 +120,49 @@ function updatePopup (x){
     popup.classList.remove('hidden')
 }
 
-
-/*function updateLs (el_id, x, num, del) {
-    let cards = document.querySelectorAll(".card");
-
-
+function updateLs () {
+    let cities = document.querySelectorAll('h2') 
+    
     let arr = [];
 
-    cards.forEach((card) => {
+    cities.forEach(x => {
         arr.push({
-            id: card.id,
-            name: x,
-            likes: {
-                num
-            },
-            delete: del = false,
+            name: x.innerHTML
         })
-
-        
     })
+
+    localStorage.setItem('posts', JSON.stringify(arr))
+
+    
 }
-*/
+
+function likeLs () {
+    let likesEl = document.querySelectorAll('span');
+
+    let likeArr = [];
+
+    likesEl.forEach(um => {
+        likeArr.push({
+            like: um.innerText
+        })
+    })
+    localStorage.setItem('likes', JSON.stringify(likeArr))
+}
+
+/*function updateLsId (card_id) {
+
+    let numArr = [];
+    
+    card_id.forEach(x => {
+        numArr.push({
+            id: x
+        })
+    })
+    localStorage.setItem('id', JSON.stringify(numArr))
+}*/
+
+//console.log(postStorage)
+//console.log(likeStorage[0].like)
 
 
 
